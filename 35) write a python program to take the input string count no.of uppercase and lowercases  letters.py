93302eeb01440e4e9e4767a80da8a65d6de811ef
parvati1230 <144988950+parvati1230@users.noreply.github.com>
2024-07-20 15:49:08 +0530

    Update

---
+++ 35) write a python program to take the input string count no.of uppercase and lowercases  letters.py
@@ -0,0 +1,11 @@
+def countupper(s):
+    upper=0
+    lower=0
+    for i in s:
+        if(i>='A' and i<='Z'):
+            upper+=1 
+        elif(i>='a' and i<='z'): 
+            lower+=1
+    print('upper is',upper)
+    print('Lower is',lower)
+countupper('PyThOn')
