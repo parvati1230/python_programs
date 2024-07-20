7aa9ca7b51fdf11a826b6d7d850c5dc758e8e144
parvati1230 <144988950+parvati1230@users.noreply.github.com>
2024-07-20 15:50:41 +0530

    Update

---
+++ 36) write a python program to check the prime or not using functions.py
@@ -0,0 +1,10 @@
+def prime(n):
+    count=1
+    for i in range(2, n+1): 
+        if (n%i==0):
+            count=count+1
+    if(count>1):
+        print("not a prime") 
+    else:
+        print("Prime")
+prime(9)
